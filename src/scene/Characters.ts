3701c9a1573a411d26cfe3f679348a2761d12450
idomshi <56523771+idomshi@users.noreply.github.com>
2023-12-24 11:57:11 +0900

    add a second character to the screen

--- src/scene/Characters.ts
+++ src/scene/Characters.ts
@@ -1,4 +1,5 @@
 import char_0 from "../../assets/character/0.webp";
+import char_1 from "../../assets/character/1.webp";
 
 export class Characters extends Phaser.Scene {
   private character!: Phaser.GameObjects.Container;
@@ -8,13 +9,25 @@ export class Characters extends Phaser.Scene {
 
   preload() {
     this.load.image("char-0", char_0);
+    this.load.image("char-1", char_1);
   }
 
   create() {
     const { width, height } = this.game.canvas;
-    this.character = this.add.container(width / 2, height / 2);
-    const image = this.add.image(0, 0, "char-0").setScale(2);
-    image.setName("0");
-    this.character.add(image);
+    const [cx, cy] = [width >> 1, height >> 1];
+    this.character = this.add.container(cx, cy);
+
+    // 立ち絵のx座標を計算する。
+    const chars = ["char-0", "char-1"];
+    const char_num = chars.length;
+    const step = Math.round(width / (char_num + 1));
+    let pos = step - cx;
+    for (let i = 0; i <= char_num; ++i) {
+      const image = this.add.image(pos, 0, chars[i])
+        .setScale(2);
+      image.setName(String(i));
+      this.character.add(image);
+      pos += step;
+    }
   }
 }
